d1b6b0e9528e88f65538984b8cf74ccc9589897d
ClovisSargenton <49842837+ClovisSargenton@users.noreply.github.com>
2020-10-30 12:34:18 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+    });
+    
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+    });
+
+    $(document).ready(function(){
+        $('.fixed-action-btn').floatingActionButton();
+    });
+});
+
+
