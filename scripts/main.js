46fcdfe51dd1221dae5ca2b612204b69387993de
ClovisSargenton <clovis.sargenton@gmail.com>
2020-11-08 17:28:30 +0100

    new version

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,22 @@ $(function() {
     $(document).ready(function(){
         $('.fixed-action-btn').floatingActionButton();
     });
+
+    
+
+    let x = window.matchMedia("(max-width: 600px)");
+
+    function centerCompLi(x){
+        if (x.matches) {
+            $('.comp_ul').addClass('center');
+        }
+        else {
+            $('.comp_ul').removeClass('center');
+        }
+    }
+
+    x.addListener(centerCompLi);
+    centerCompLi(x);
 });
 
 
